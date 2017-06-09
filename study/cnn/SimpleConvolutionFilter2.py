47df7f2b7b1d299168375a291d3f5092a83a4547
SeungHwan Kim <Super@SeungHwanui-MacBook-Pro.local>
2017-06-10 00:23:22 +0900

    TensorFlow Study

---
+++ study/cnn/SimpleConvolutionFilter2.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+data = range(18)  # 3x3x2
+# NumberHeightWidthChannel = Input(image), HWCN = Filter(w)
+image = np.reshape(data, (1, 3, 3, 2))
+image = image.astype(np.float32)
+
+# plt.imshow(image.reshape(3, 3), cmap="Greys", interpolation="nearest")
+# plt.show()
+
+sess = tf.Session()
+
+w = tf.constant([[1., 1., 1., 1.], [1., 1., 1., 1.]])
+w = tf.reshape(w, [2, 2, 2, 1])
+print(sess.run(w))
+
+conv2d = tf.nn.conv2d(image, w, strides=[1, 1, 1, 1], padding="VALID")
+cond2d_img = sess.run(conv2d)
+print(image)
+print(cond2d_img)
+print('image', image.shape)
+print('cond2d_img', cond2d_img)
