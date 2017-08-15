65d75094950ed2067e16586c42ee55184ac4b122
SeungHwan Kim <Super@SeungHwanui-MacBook-Pro.local>
2017-08-15 13:46:09 +0900

    7강 Trainning and Test && Normalized

---
+++ self/lab/7/TrainingAndTesting.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+x_data = [[1, 2, 1], [1, 3, 2], [1, 3, 4], [1, 5, 5], [1, 7, 5], [1, 2, 5], [1, 6, 6], [1, 7, 7]]
+y_data = [[0, 0, 1], [0, 0, 1], [0, 0, 1], [0, 1, 0], [0, 1, 0], [0, 1, 0], [1, 0, 0], [1, 0, 0]]
+
+x_test = [[2, 1, 1], [3, 1, 2], [3, 3, 4]]
+y_test = [[0, 0, 1], [0, 0, 1], [0, 0, 1]]
+
+X = tf.placeholder(tf.float32, [None, 3])
+Y = tf.placeholder(tf.float32, [None, 3])
+
+W = tf.Variable(tf.random_normal([3, 3]), name='weight')
+b = tf.Variable(tf.random_normal([3]), name='bias')
+
+logits = tf.matmul(X, W) + b
+hypothesis = tf.nn.softmax(logits)
+
+cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y)
+cost = tf.reduce_mean(cost_i)
+
+train = tf.train.GradientDescentOptimizer(learning_rate=100).minimize(cost)
+
+prediction = tf.argmax(hypothesis, 1)
+
+correct_prediction = tf.equal(prediction, tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, dtype=tf.float32))
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(201):
+        cost_, train_ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
+
+        print("step: ", step, "\tcost: ", cost_)
+
+    print("prediction: ", sess.run(prediction, feed_dict={X: x_test}))
+    print("accuracy: ", sess.run(accuracy, feed_dict={X: x_test, Y: y_test}))
